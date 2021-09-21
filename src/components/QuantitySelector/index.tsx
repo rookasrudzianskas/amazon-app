8dd4edc1f0edd589579bf6cc908f73b8fb84dedd
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 16:26:46 +0300

    Rokas

--- src/components/QuantitySelector/index.tsx
+++ src/components/QuantitySelector/index.tsx
@@ -4,10 +4,20 @@ import tw from "tailwind-react-native-classnames";
 import {Entypo, Ionicons} from "@expo/vector-icons";
 
 const QuantitySelector = () => {
+
+    const onMinus = () => {
+
+    }
+
+    const onPlus = () => {
+
+    }
+
+
     return (
         <View>
             <View style={tw`flex flex-row items-center`}>
-                <TouchableOpacity activeOpacity={0.8}>
+                <TouchableOpacity activeOpacity={0.8} onPress={onMinus}>
                     <View style={tw`bg-gray-300 px-2 py-1 rounded-md`}>
                         <Ionicons name="ios-trash-bin" size={24} color="black" />
                     </View>
@@ -17,7 +27,7 @@ const QuantitySelector = () => {
                     <Text style={tw`px-3`}>1</Text>
                 </View>
 
-                <TouchableOpacity activeOpacity={0.8}>
+                <TouchableOpacity onPress={onPlus} activeOpacity={0.8}>
                     <View style={tw`bg-gray-300 px-2 py-1 rounded-md`}>
                         <Entypo name="plus" size={24} color="black" />
                     </View>
