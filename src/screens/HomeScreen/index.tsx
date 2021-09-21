bec7bb99b9db8b02a5ef61ea473630b2fd804d48
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 12:55:37 +0300

    Rokas

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text} from "react-native";
+import tw from "tailwind-react-native-classnames";
+import styles from './style';
 
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Rokas</Text>
+        {/*    product component */}
+            <View style={tw``}>
+
+            </View>
         </View>
     );
 };
