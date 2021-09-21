3bbcacabd1f8c8c6c54fce1f641a0c65a29f251e
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 12:14:35 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import tailwind from "tailwind-rn";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       <StatusBar style="auto" />
+
+      <View>
+        <HomeScreen />
+      </View>
     </View>
   );
 }
