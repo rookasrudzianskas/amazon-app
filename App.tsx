ead9197c802f9d2c962740f31eaebe5b64dc88e1
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 11:44:14 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import tw from "tailwind-react-native-classnames";
+import tailwind from "tailwind-rn";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={tw`text-xl font-bold`}>Amazon 🔥!</Text>
+      <SafeAreaView style={tailwind('h-full')}>
+        <View style={tailwind('pt-12 items-center')}>
+          <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
+            <Text style={tailwind('text-blue-800 font-semibold')}>
+              Hello, let's build an amazon!
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
     </View>
   );
