144f4db998761aecd1a16b5dec6b7697b120ac98
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 13:18:00 +0300

    Rokas

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, Image} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import styles from './style';
+import tailwind from "tailwind-rn";
 
 const HomeScreen = () => {
     return (
-        <View>
-        {/*    product component */}
-            <View style={tw``}>
+        <View style={tw`m-3`}>
+            {/*    product component */}
+                <View>
+                <View style={tw`flex flex-row mt-12 px-2 border-2 border-gray-200 rounded-md items-center`}>
+                    <View style={tw``}>
+                        <Image style={[styles.image, tw``]} source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/mouse1.jpg'}} />
+                    </View>
 
+                    <View style={tw`flex ml-5 mt-5`}>
+                        <Text numberOfLines={3}  style={[styles.textSize, tw`text-lg`]}>Clean Architecture: A Craftsman's Guide to Software  Structure and Design</Text>
+
+                        <View style={tw``}>
+
+                        </View>
+
+                        <View style={tw`flex flex-row mt-2 mb-2 items-center`}>
+                            <Text style={tw`text-black text-lg`}>from </Text>
+                            <Text style={tw`text-black text-xl font-bold`}>$123 </Text>
+                        </View>
+                    </View>
+                </View>
             </View>
         </View>
     );
--- src/screens/HomeScreen/style.ts
+++ src/screens/HomeScreen/style.ts
@@ -1,7 +1,15 @@
-import {StyleSheet} from 'react-native';
+import {Dimensions, StyleSheet} from 'react-native';
+
 
 const styles = StyleSheet.create({
-
+    image: {
+        width: Dimensions.get('window').width / 3,
+        height: Dimensions.get('window').height / 10,
+        resizeMode: 'contain'
+    },
+    textSize: {
+        maxWidth: Dimensions.get('window').width / 2,
+    }
 });
 
 export default styles;
