3d371a72ad5aeaa637264c1d2088c1f2ac92b8b2
Rokas <rookas.rudzenskas@gmail.com>
2021-09-22 15:53:47 +0300

    Rokas

--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -15,7 +15,7 @@ const ImageCarousel = ({images}: ImageCarouselProps) => {
         if (viewableItems.length > 0) {
             setActiveIndex(viewableItems[0].index || 0);
         }
-        console.log(viewableItems);
+        // console.log(viewableItems);
     }, []);
 
     return (
