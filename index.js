fe7e1f3f86b403ce4cf4a034e174a9e1ab235ca9
Rokas <rookas.rudzenskas@gmail.com>
2021-09-24 11:13:33 +0300

    Rokas

---
+++ index.js
@@ -0,0 +1,13 @@
+import { AppRegistry, Platform } from 'react-native';
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+
+AppRegistry.registerComponent('main', () => App);
+
+if (Platform.OS === 'web') {
+    const rootTag = document.getElementById('root') || document.getElementById('main');
+    AppRegistry.runApplication('main', { rootTag });
+}
