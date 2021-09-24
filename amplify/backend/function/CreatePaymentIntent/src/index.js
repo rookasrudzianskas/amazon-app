967223ec453ebd286d1a33890462d6ca6c8ab816
Rokas <rookas.rudzenskas@gmail.com>
2021-09-24 12:33:50 +0300

    Rokas

--- amplify/backend/function/CreatePaymentIntent/src/index.js
+++ amplify/backend/function/CreatePaymentIntent/src/index.js
@@ -1,4 +1,15 @@
+const stripe = require('stripe')('sk_test_51JdAM1HPTF8FvliL7HaObPB6TM2C3Tw9rpibIpnt81wQunc31G4HLMsDH1Pt0tNCZSuJclza3eMo1j6tnlZN24ez00hDVYAP2z');
 
+// event
+// {
+//   "typeName": "Query" | "Mutation", /* Filled dynamically based on @function usage location */
+//   "fieldName": "createPaymentMethod", /* Filled dynamically based on @function usage location */
+//   "arguments": { amount  /* GraphQL field arguments via $ctx.arguments */ },
+//   "identity": { /* AppSync identity object via $ctx.identity */ },
+//   "source": { /* The object returned by the parent resolver. E.G. if resolving field 'Post.comments', the source is the Post object. */ },
+//   "request": { /* AppSync request object. Contains things like headers. */ },
+//   "prev": { /* If using the built-in pipeline resolver support, this contains the object returned by the previous function. */ },
+// }
 
 exports.handler = async (event) => {
     const { typeName, arguments } = event;
@@ -11,5 +22,15 @@ exports.handler = async (event) => {
         throw new Error('Amount is required');
     }
 
+    // create payment intent
+    // create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: arguments.amount,
+        currency: 'usd'
+    });
+
+    return {
+        clientSecret: paymentIntent.client_secret,
+    }
 
 };
